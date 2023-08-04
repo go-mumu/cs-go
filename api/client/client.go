406f31f57dc24b39b22bbb131e90637fb8b50d6c
Weiyixmm <634482545@qq.com>
2023-08-04 17:14:42 +0800

    client

--- api/client/client.go
+++ api/client/client.go
@@ -3,31 +3,37 @@
  * User: liyw<634482545@qq.com>
  * Date: 2023-07-28
  * File: client.go
- * Desc:
+ * Desc: service client
  */
 
 package client
 
 import (
-	"context"
+	"github.com/go-mumu/cs-go/library/config"
 	"github.com/go-mumu/cs-go/proto/pb"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
 
 type ServiceClient struct {
+	Config *config.Config
 }
 
-func NewServiceClient() *ServiceClient {
-	return &ServiceClient{}
+func NewServiceClient(conf *config.Config) *ServiceClient {
+	return &ServiceClient{
+		Config: conf,
+	}
 }
 
-func (c *ServiceClient) UserClient(ctx context.Context) pb.UserServiceClient {
-	return pb.NewUserServiceClient(c.grpcDial(ctx))
+func (c *ServiceClient) UserClient() pb.UserServiceClient {
+	return pb.NewUserServiceClient(c.dial())
 }
 
-func (c *ServiceClient) grpcDial(ctx context.Context) *grpc.ClientConn {
-	conn, err := grpc.DialContext(ctx, "127.0.0.1:8992", grpc.WithTransportCredentials(insecure.NewCredentials()))
+func (c *ServiceClient) dial() *grpc.ClientConn {
+	conn, err := grpc.Dial(
+		c.Config.Client.ServiceAddr+c.Config.Rpc.GrpcAddr,
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
 	if err != nil {
 		return nil
 	}
